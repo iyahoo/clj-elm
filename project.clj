242f15bcd0a3b40f4ffd3112ebfea8d19532af43
iyahoo <s1200191@gmail.com>
2015-10-26 17:14:45 +0900

    [Update] cider-nrepl

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[cider/cider-nrepl "0.9.1"]
+  :plugins [[cider/cider-nrepl "0.10.0-snapshot"]
             [lein-midje "3.1.3"]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  ;; [org.clojure/core.match "0.3.0-alpha4"]
