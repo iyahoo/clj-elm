a2d76bdf08c40a33a64f7b8e23226545830bf70f
iyahoo <s1200191@gmail.com>
2015-10-26 17:15:08 +0900

    [Delete] type annotation

--- src/clj_elm/data.clj
+++ src/clj_elm/data.clj
@@ -8,74 +8,74 @@
 
 (defrecord DataSet [classes features])
 
-(defn ^clojure.lang.PersistentVector parse-lib-svm-data
-  ([^clojure.lang.PersistentVector line]
+(defn parse-lib-svm-data
+  ([line]
    {:pre [(integer? (first line)) (map? (last line))]}
    (let [[class featmap] line
          fnum (count featmap)
          features (map #(get featmap %) (range 1 (inc fnum)))]
      [class features])))
 
-(defn ^DataSet data-set
+(defn data-set
   "Make DataSet from incanter-form or csv dataset. Cidx is column number of class."
-  ([^incanter.core.Dataset dataset ^Integer cidx]
+  ([cidx]
    {:pre [(c/dataset? dataset)]}
    (DataSet. (pmap int (c/to-vect (c/$ :all cidx dataset))) (c/to-vect (c/$ [:not cidx] dataset)))))
 
-(defn ^DataSet read-dataset
-  ([^String path ^Integer cidx & {:keys [^Boolean header] :or {header false}}]
+(defn read-dataset
+  ([& {:keys [header] :or {header false}}]
    {:pre [(string? path) (integer? cidx)]}   
    (-> (io/read-dataset path :delim \, :header header)
        (data-set cidx))))
 
-(defn ^DataSet read-dataset-lib-svm
-  ([^String path]
+(defn read-dataset-lib-svm
+  ([path]
    {:pre [(string? path)]}
    (->> (svm/read-dataset path)
         (pmap parse-lib-svm-data)
         (#(DataSet. (map first %) (map second %))))))
 
-(defn ^DataSet concat-dataset
-  ([^DataSet dsa ^DataSet dsb]
+(defn concat-dataset
+  ([dsb]
    {:pre [(instance? DataSet dsa) (instance? DataSet dsb)]}
    (DataSet. (concat (:classes dsa) (:classes dsb))
              (concat (:features dsa) (:features dsb)))))
 
-(defn ^DataSet shuffle-dataset
-  ([^DataSet dataset]
+(defn shuffle-dataset
+  ([dataset]
    {:pre [(instance? DataSet dataset)]}
    (-> (pmap vector (:classes dataset) (:features dataset))
        (shuffle)
        (#(DataSet. (pmap first %) (pmap second %))))))
 
-(defn ^Integer num-of-feature 
-  ([^DataSet dataset]
+(defn num-of-feature 
+  ([dataset]
    {:pre [(coll? dataset)]}
    (count (first (:features dataset)))))
 
 (defn ith-feature-list 
-  ([^DataSet dataset ^Integer i]
+  ([i]
    {:pre [(coll? dataset) (integer? i)]}
    (c/$ i (c/to-dataset dataset))))
 
-(defn ^clojure.lang.PersistentVector each-ith-f 
-  ([f ^DataSet dataset]
+(defn each-ith-f 
+  ([f dataset]
    (->> (range 0 (count (first dataset)))
         (pmap #(f (ith-feature-list dataset %)))
         (vec))))
 
-(defn ^clojure.lang.PersistentVector each-ith-mean 
-  ([^DataSet dataset]
+(defn each-ith-mean 
+  ([dataset]
    {:pre [(coll? dataset)]}
    (each-ith-f st/mean dataset)))
 
-(defn ^clojure.lang.PersistentVector each-ith-sd 
-  ([^DataSet dataset]
+(defn each-ith-sd 
+  ([dataset]
    {:pre [(coll? dataset)]}
    (each-ith-f st/sd dataset)))
 
-(defn ^clojure.lang.PersistentVector normalize 
-  ([^DataSet dataset]
+(defn 
+  ([dataset]
    {:pre [(coll? dataset)]}
    (let [means (each-ith-mean dataset)
          sds (each-ith-sd dataset)]    
@@ -83,12 +83,12 @@
           (pmap #(map - % means))
           (pmap #(map / % sds))))))
 
-(defn ^clojure.lang.PersistentVector remove-list
-  ([^clojure.lang.PersistentVector lst ^Integer a ^Integer b]
+(defn remove-list
+  ([b]
    {:pre [(coll? lst) (integer? a) (integer? b) (> b a)]}
    (concat (take a lst) (drop (inc b) lst))))
 
-(defn ^clojure.lang.PersistentVector extract-list
-  ([^clojure.lang.PersistentVector lst ^Integer a ^Integer b]
+(defn extract-list
+  ([b]
    {:pre [(coll? lst) (integer? a) (integer? b) (> b a)]}
    (take (inc (- b a)) (drop a lst))))
