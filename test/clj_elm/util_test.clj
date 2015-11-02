579e1f0bbc0c003bf1a5e3565c86f67e2149e82b
iyahoo <s1200191@gmail.com>
2015-11-02 16:58:58 +0900

    [Add] file util_test, test reftype?

---
+++ test/clj_elm/util_test.clj
@@ -0,0 +1,13 @@
+(ns clj-elm.core-test
+  (:require [clj-elm.util :refer :all]
+            [midje.sweet :refer :all]
+            [midje.repl :refer (autotest load-facts)]))
+
+(facts "test-reftype?"
+  (fact "(reftype? obj)"
+    (reftype? 3)
+    => false
+    (reftype? (atom 1))
+    => true
+    (reftype? (ref 3))
+    => true))
