4dab0c7623826d4e9677e35be87a83f431e20001
iyahoo <s1200191@gmail.com>
2015-11-02 16:58:45 +0900

    [Add] fun reftype?

--- src/clj_elm/util.clj
+++ src/clj_elm/util.clj
@@ -3,3 +3,7 @@
 (defn printfl [obj]
   {:post [(coll? %) (nil? (first %))]}
   [(print obj) (flush)])
+
+(defn reftype? [obj]
+  (or (instance? clojure.lang.Ref obj)
+      (instance? clojure.lang.Atom obj)))
