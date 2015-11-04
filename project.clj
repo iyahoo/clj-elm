459e5048e6561961edd04584b90b1840caf69e6c
iyahoo <s1200191@gmail.com>
2015-11-05 06:45:08 +0900

    [Remove] core.typed

--- project.clj
+++ project.clj
@@ -8,7 +8,6 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/tools.nrepl "0.2.10"]
-                 [org.clojure/core.typed "0.3.0"]
                  [incanter "1.5.6"]
                  [midje "1.7.0-beta1"]
                  [svm-clj "0.1.3"]
