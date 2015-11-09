17b84bafebfd31cfc66787e31569109b8443ebfa
iyahoo <s1200191@gmail.com>
2015-11-10 01:21:58 +0900

    [Fix] for coveralls

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-            [lein-midje "3.1.3"]]
+            [lein-midje "3.1.3"]
+            [lein-cloverage "1.0.2"]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/tools.nrepl "0.2.10"]
