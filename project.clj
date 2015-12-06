1c955a4bbfc58b1f29fb57398bae5c7da5772f1a
iyahoo <s1200191@gmail.com>
2015-12-06 17:23:00 +0900

    [Update] clojure-nrepl 0.10.0-SNAPSHOT -> 0.10.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+  :plugins [[cider/cider-nrepl "0.10.0"]
             [lein-midje "3.1.3"]
             [lein-cloverage "1.0.2"]]
   :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]}
